962b50a5f6d3a8993e8348ce9eaeac7ecc5a8b96
Kingsley Omoaka-Iyorah <kingsley.omiy@gmail.com>
2024-07-28 19:14:44 +0100

    added loading spinner

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,15 +1,14 @@
-import { BallTriangle } from "react-loader-spinner";
+import { Circles } from "react-loader-spinner";
 
 const Loading = () => {
   return (
     <div className="flex justify-center items-center h-screen">
-      <BallTriangle
+      <Circles
         visible={true}
         height="100"
         width="100"
-        radius={5}
         color="#2563EB"
-        ariaLabel="ball-triangle-loading"
+        ariaLabel="circles-loading"
         wrapperStyle={{}}
         wrapperClass=""
       />
